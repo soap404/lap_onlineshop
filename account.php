97b03a6219f9c312b691ab2e97d05c3a36ba5bf3
Soap404 <alarjawia@gmail.com>
2024-09-25 09:57:11 +0200

    CREATE ACCOUNT PAGE WITH 2 BUTTONS ORDER Account settings

---
+++ account.php
@@ -0,0 +1,9 @@
+<?php require_once 'templates/header.php'; ?>
+
+<a type="button" class="btn btn-success">Orders</a>
+
+<a href="account_settings.php" class="btn btn-primary">Account settings</a>
+
+
+
+<?php require_once 'templates/footer.php'; ?>
